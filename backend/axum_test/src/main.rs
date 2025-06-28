f61cbf164ea8d9796b660965263029154950a82a
BaranBaranDev <auniversty@gmail.com>
2025-06-28 16:26:10 +0300

    feat: Initialize Axum web server with vehicle routes

---
+++ backend/axum_test/src/main.rs
@@ -0,0 +1,36 @@
+use axum::{routing::{get, post}, Router};
+
+/*
+    -router ile json formatında ki girdileri belirliyoruz burada ki / anasayfayı işaret ediyor
+
+
+*/
+
+#[tokio::main]
+async fn main() {
+    // Router oluşturduk
+    let vehicle_router = Router::new()
+    .route("/vehicle", 
+    get(vehicle_get).
+    post(vehicle_post));
+
+    // ip ve port listener(tcp) tanımlıyoruz
+    let addr = "0.0.0.0.6570";
+    let listener = tokio::net::TcpListener::bind(addr)
+    .await
+    .unwrap();
+ 
+    // axum ile web sunucusunu başlatıyoruz
+    axum::serve(listener, vehicle_router)
+    .await
+    .unwrap();
+
+}
+
+async fn vehicle_get(){
+
+}
+
+async fn vehicle_post(){
+
+}
